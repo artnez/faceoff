446a86af192cd3682904c4c92ccfcd7f53232e21
Artem Nezvigin <artem@artnez.com>
2012-04-03 00:03:53 -0700

    typo fix

--- src/faceoff/config.py
+++ src/faceoff/config.py
@@ -1,5 +1,5 @@
 """
-Stores defaul application configuration settings.
+Stores default application configuration settings.
 
 Copyright: (c) 2012 Artem Nezvigin <artem@artnez.com>
 License: MIT, see LICENSE for details
